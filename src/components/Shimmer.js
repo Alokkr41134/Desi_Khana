386464480567480dd6d93d5e59a5cad11794754f
Alokkr41134 <alokarya408@gmail.com>
2025-01-04 00:43:59 +0530

    Desi Khana

---
+++ src/components/Shimmer.js
@@ -0,0 +1,27 @@
+const Shimmer = () => {
+  return (
+
+    <div className="flex flex-wrap mt-40 ">
+      <div className="shimmer-card m-4 ml-6 p-4 w-[250px] h-[450px] rounded-lg bg-gray-100 "></div>
+      <div className="shimmer-card m-4 ml-6 p-4 w-[250px] h-[450px] rounded-lg bg-gray-100 hover:bg-gray-200 "></div>
+      <div className="shimmer-card m-4 ml-6 p-4 w-[250px] h-[450px] rounded-lg bg-gray-100 hover:bg-gray-200 "></div>
+      <div className="shimmer-card m-4 ml-6 p-4 w-[250px] h-[450px] rounded-lg bg-gray-100 hover:bg-gray-200 "></div>
+      <div className="shimmer-card m-4 ml-6 p-4 w-[250px] h-[450px] rounded-lg bg-gray-100 hover:bg-gray-200 "></div>
+      <div className="shimmer-card m-4 ml-6 p-4 w-[250px] h-[450px] rounded-lg bg-gray-100 hover:bg-gray-200 "></div>
+      <div className="shimmer-card m-4 ml-6 p-4 w-[250px] h-[450px] rounded-lg bg-gray-100 hover:bg-gray-200 "></div>
+      <div className="shimmer-card m-4 ml-6 p-4 w-[250px] h-[450px] rounded-lg bg-gray-100 hover:bg-gray-200 "></div>
+      <div className="shimmer-card m-4 ml-6 p-4 w-[250px] h-[450px] rounded-lg bg-gray-100 hover:bg-gray-200  "></div>
+      <div className="shimmer-card m-4 ml-6 p-4 w-[250px] h-[450px] rounded-lg bg-gray-100 hover:bg-gray-200 "></div>
+      <div className="shimmer-card m-4 ml-6 p-4 w-[250px] h-[450px] rounded-lg bg-gray-100 hover:bg-gray-200 "></div>
+      <div className="shimmer-card m-4 ml-6 p-4 w-[250px] h-[450px] rounded-lg bg-gray-100 hover:bg-gray-200 "></div>
+      <div className="shimmer-card m-4 ml-6 p-4 w-[250px] h-[450px] rounded-lg bg-gray-100 hover:bg-gray-200 "></div>
+      <div className="shimmer-card m-4 ml-6 p-4 w-[250px] h-[450px] rounded-lg bg-gray-100 hover:bg-gray-200 "></div>
+      <div className="shimmer-card m-4 ml-6 p-4 w-[250px] h-[450px] rounded-lg bg-gray-100 hover:bg-gray-200 "></div>
+      <div className="shimmer-card m-4 ml-6 p-4 w-[250px] h-[450px] rounded-lg bg-gray-100 hover:bg-gray-200 "></div>
+      <div className="shimmer-card m-4 ml-6 p-4 w-[250px] h-[450px] rounded-lg bg-gray-100 hover:bg-gray-200 "></div>
+      <div className="shimmer-card m-4 ml-6 p-4 w-[250px] h-[450px] rounded-lg bg-gray-100 hover:bg-gray-200 "></div>
+    </div>
+  );
+};
+
+export default Shimmer;
